efe56ee14dccea6fc22c6290f474abb644a7cbaf
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-05-31 15:21:06 -0600

    :sparkles: Init sites config

---
+++ src/constants/site-info.ts
@@ -0,0 +1,4 @@
+export const SITE_INFO = {
+  title: 'Interstellar Code',
+  description: 'Interstellar Code is a software development blog, in where you can discover incredible tricks and tips to increase your programming skills',
+};
