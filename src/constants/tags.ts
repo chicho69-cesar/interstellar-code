b0e197ca3e583fe66f8312de16859786e0c7b791
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-06-01 18:20:48 -0600

    :sparkles: Add tags for blog posts

---
+++ src/constants/tags.ts
@@ -0,0 +1,99 @@
+// import AWS from '@/icons/tech/AWS.astro'
+// import AstroIcon from '@/icons/tech/AstroIcon.astro'
+import CSS from '@/icons/tech/CSS.astro'
+import Docker from '@/icons/tech/Docker.astro'
+import Express from '@/icons/tech/Express.astro'
+// import Flask from '@/icons/tech/Flask.astro'
+import JavaScript from '@/icons/tech/JavaScript.astro'
+// import MongoDB from '@/icons/tech/MongoDB.astro'
+// import MySQL from '@/icons/tech/MySQL.astro'
+// import NextJS from '@/icons/tech/NextJS.astro'
+import Node from '@/icons/tech/Node.astro'
+// import Python from '@/icons/tech/Python.astro'
+// import React from '@/icons/tech/React.astro'
+// import Spring from '@/icons/tech/Spring.astro'
+// import Tailwind from '@/icons/tech/Tailwind.astro'
+import TypeScript from '@/icons/tech/TypeScript.astro'
+
+export const TAGS: any = {
+  // ASTRO: {
+  //   name: 'Astro',
+  //   class: 'bg-[#000] text-white',
+  //   icon: AstroIcon
+  // },
+  // AWS: {
+  //   name: 'AWS',
+  //   class: 'bg-[#FF9900bb] text-white',
+  //   icon: AWS,
+  // },
+  DOCKER: {
+    name: 'Docker',
+    class: 'bg-[#008fe299] text-white',
+    icon: Docker
+  },
+  // FLASK: {
+  //   name: 'Flask',
+  //   class: 'bg-[#000] text-white',
+  //   icon: Flask,
+  // },
+  // MONGODB: {
+  //   name: 'MongoDB',
+  //   class: 'bg-[#00640099] text-white',
+  //   icon: MongoDB,
+  // },
+  // MYSQL: {
+  //   name: 'MySQL',
+  //   class: 'bg-[#f57f17cc] text-white',
+  //   icon: MySQL
+  // },
+  // NEXT: {
+  //   name: 'Next.js',
+  //   class: 'bg-black text-white',
+  //   icon: NextJS,
+  // },
+  // PYTHON: {
+  //   name: 'Python',
+  //   class: 'bg-[#3572A599] text-white',
+  //   icon: Python,
+  // },
+  // REACT: {
+  //   name: 'React',
+  //   class: 'bg-[#61dafb55] text-black',
+  //   icon: React
+  // },
+  // SPRING: {
+  //   name: 'Spring',
+  //   class: 'bg-[#b3e5fc] text-black',
+  //   icon: Spring
+  // },
+  // TAILWIND: {
+  //   name: 'Tailwind CSS',
+  //   class: 'bg-[#003159] text-white',
+  //   icon: Tailwind,
+  // },
+  CSS: {
+    name: 'CSS',
+    class: 'bg-[#66339966] text-white',
+    icon: CSS,
+  },
+  EXPRESS: {
+    name: 'Express',
+    class: 'bg-[#000] text-white',
+    icon: Express,
+  },
+  JAVASCRIPT: {
+    name: 'JavaScript',
+    class: 'bg-[#f7df1eaa] text-black',
+    icon: JavaScript,
+  },
+  TYPESCRIPT: {
+    name: 'TypeScript',
+    class: 'bg-[#3178c699] text-white',
+    icon: TypeScript,
+  },
+  NODE: {
+    name: 'Node',
+    class: 'bg-[#8CC84B99] text-white',
+    icon: Node,
+  },
+}
