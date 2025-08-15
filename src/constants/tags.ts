85648c073e12d331bce04b95323acfd5dae87499
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-15 11:07:25 -0600

    :bento: Add icons with svg format

--- src/constants/tags.ts
+++ src/constants/tags.ts
@@ -1,111 +1,110 @@
-// import AWS from '@/icons/tech/AWS.astro'
-// import AstroIcon from '@/icons/tech/AstroIcon.astro'
-import CSS from '@/icons/tech/CSS.astro'
-import Docker from '@/icons/tech/Docker.astro'
-import Express from '@/icons/tech/Express.astro'
-import HTML from '@/icons/tech/HTML.astro'
-// import Flask from '@/icons/tech/Flask.astro'
-import JavaScript from '@/icons/tech/JavaScript.astro'
-// import MongoDB from '@/icons/tech/MongoDB.astro'
-// import MySQL from '@/icons/tech/MySQL.astro'
-// import NextJS from '@/icons/tech/NextJS.astro'
-import Node from '@/icons/tech/Node.astro'
-// import Python from '@/icons/tech/Python.astro'
-import React from '@/icons/tech/React.astro'
-import ReactRouter from '@/icons/tech/ReactRouter.astro'
-// import Spring from '@/icons/tech/Spring.astro'
-// import Tailwind from '@/icons/tech/Tailwind.astro'
-import TypeScript from '@/icons/tech/TypeScript.astro'
+import AstroIcon from '@/assets/frontend/astro.svg'
+import AWS from '@/assets/dev/aws.svg'
+import CSS from '@/assets/frontend/css.svg'
+import Docker from '@/assets/dev/docker.svg'
+import Express from '@/assets/backend/expressjs.svg'
+import Flask from '@/assets/backend/flask.svg'
+import HTML from '@/assets/frontend/html.svg'
+import JavaScript from '@/assets/frontend/javascript.svg'
+import MongoDB from '@/assets/databases/mongodb.svg'
+import MySQL from '@/assets/databases/mysql.svg'
+import NextJS from '@/assets/frontend/nextjs.svg'
+import Node from '@/assets/backend/nodejs.svg'
+import Python from '@/assets/backend/python.svg'
+import React from '@/assets/frontend/react.svg'
+import ReactRouter from '@/assets/frontend/react-router.svg'
+import Tailwind from '@/assets/frontend/tailwindcss.svg'
+import TypeScript from '@/assets/frontend/typescript.svg'
 
 export const TAGS: any = {
-  // ASTRO: {
-  //   name: 'Astro',
-  //   class: 'bg-[#000] text-white',
-  //   icon: AstroIcon
-  // },
-  // AWS: {
-  //   name: 'AWS',
-  //   class: 'bg-[#FF9900bb] text-white',
-  //   icon: AWS,
-  // },
-  DOCKER: {
-    name: 'Docker',
-    class: 'bg-[#008fe299] text-white',
-    icon: Docker
+  ASTRO: {
+    name: 'Astro',
+    class: 'bg-[#FF5D00bb] text-white',
+    icon: AstroIcon
   },
-  // FLASK: {
-  //   name: 'Flask',
-  //   class: 'bg-[#000] text-white',
-  //   icon: Flask,
-  // },
-  // MONGODB: {
-  //   name: 'MongoDB',
-  //   class: 'bg-[#00640099] text-white',
-  //   icon: MongoDB,
-  // },
-  // MYSQL: {
-  //   name: 'MySQL',
-  //   class: 'bg-[#f57f17cc] text-white',
-  //   icon: MySQL
-  // },
-  // NEXT: {
-  //   name: 'Next.js',
-  //   class: 'bg-black text-white',
-  //   icon: NextJS,
-  // },
-  // PYTHON: {
-  //   name: 'Python',
-  //   class: 'bg-[#3572A599] text-white',
-  //   icon: Python,
-  // },
-  REACT: {
-    name: 'React',
-    class: 'bg-[#61dafb55] text-white',
-    icon: React
-  },
-  // SPRING: {
-  //   name: 'Spring',
-  //   class: 'bg-[#b3e5fc] text-black',
-  //   icon: Spring
-  // },
-  // TAILWIND: {
-  //   name: 'Tailwind CSS',
-  //   class: 'bg-[#003159] text-white',
-  //   icon: Tailwind,
-  // },
-  HTML: {
-    name: 'HTML',
-    class: 'bg-[#e34c26aa] text-white',
-    icon: HTML,
+  AWS: {
+    name: 'AWS',
+    class: 'bg-[#FF9900bb] text-white',
+    icon: AWS,
   },
   CSS: {
     name: 'CSS',
     class: 'bg-[#66339966] text-white',
     icon: CSS,
   },
+  DOCKER: {
+    name: 'Docker',
+    class: 'bg-[#008fe299] text-white',
+    icon: Docker
+  },
   EXPRESS: {
     name: 'Express',
-    class: 'bg-[#000] text-white',
+    class: 'bg-[#0007] text-white',
     icon: Express,
+  },
+  FLASK: {
+    name: 'Flask',
+    class: 'bg-[#000] text-white',
+    icon: Flask,
+  },
+  HTML: {
+    name: 'HTML',
+    class: 'bg-[#e34c26aa] text-white',
+    icon: HTML,
   },
   JAVASCRIPT: {
     name: 'JavaScript',
     class: 'bg-[#f7df1eaa] text-black',
     icon: JavaScript,
   },
-  TYPESCRIPT: {
-    name: 'TypeScript',
-    class: 'bg-[#3178c699] text-white',
-    icon: TypeScript,
+  MONGODB: {
+    name: 'MongoDB',
+    class: 'bg-[#00640099] text-white',
+    icon: MongoDB,
+  },
+  MYSQL: {
+    name: 'MySQL',
+    class: 'bg-[#f57f17cc] text-white',
+    icon: MySQL
+  },
+  NEXT: {
+    name: 'Next.js',
+    class: 'bg-black text-white',
+    icon: NextJS,
   },
   NODE: {
     name: 'Node',
     class: 'bg-[#8CC84B99] text-white',
     icon: Node,
   },
+  PYTHON: {
+    name: 'Python',
+    class: 'bg-[#3572A599] text-white',
+    icon: Python,
+  },
+  REACT: {
+    name: 'React',
+    class: 'bg-[#61dafb55] text-black',
+    icon: React
+  },
   'REACT ROUTER': {
     name: 'React Router',
     class: 'bg-[#CA424599] text-white',
     icon: ReactRouter,
-  }
+  },
+  TYPESCRIPT: {
+    name: 'TypeScript',
+    class: 'bg-[#3178c699] text-white',
+    icon: TypeScript,
+  },
+  TAILWIND: {
+    name: 'Tailwind CSS',
+    class: 'bg-[#003159] text-white',
+    icon: Tailwind,
+  },
+  // SPRING: {
+  //   name: 'Spring',
+  //   class: 'bg-[#b3e5fc] text-black',
+  //   icon: Spring
+  // },
 }
